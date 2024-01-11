fb009228ca645ceae2446988d4d8996bc690feed
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-11 15:47:27 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePOOFuncionario.Entidade
+{
+  internal class Funcionario
+  {
+    public string Nome;
+    public double SalarioBruto;
+    public double Imposto;
+
+    public double SalarioLiquido() {
+      return this.SalarioBruto - Imposto;
+    }
+
+    public void AumentarSalario(double porcentagem) {
+      double aumento;
+      aumento = (this.SalarioBruto * porcentagem) /100;
+      this.SalarioBruto += aumento;
+    }
+
+    public override string ToString()
+    {
+       return this.Nome + ", $"+ this.SalarioLiquido();
+
+
+    }
+
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsolePOOFuncionario.Entidade;
+using System.Globalization;
+
+Funcionario funcionario = new Funcionario();
+
+Console.Write("Informe o nome do funcionario:");
+funcionario.Nome = Console.ReadLine();
+
+Console.Write("Informe o salário Bruto:");
+funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Informe o imposto: ");
+funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Funcionario :" + funcionario);
+Console.Write("Digite a porcetagem para aumentar o salário: ");
+
+funcionario.AumentarSalario(double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture));
+Console.WriteLine("Dados Atualizados " + funcionario);
+
