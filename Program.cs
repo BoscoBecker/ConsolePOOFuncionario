30d4f5a75f976f7a1aa160d631e28eb7f0137412
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-11 15:47:27 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsolePOOFuncionario.Entidade;
+using System.Globalization;
+// See https://aka.ms/new-console-template for more information
+
+Funcionario funcionario = new Funcionario();
+
+Console.Write("Informe o nome do funcionario:");
+funcionario.Nome = Console.ReadLine();
+
+Console.Write("Informe o salário Bruto:");
+funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Informe o imposto: ");
+funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Funcionario :" + funcionario);
+Console.WriteLine("Digite a porcetagem para aumentar o salário: ");
+
+funcionario.AumentarSalario(double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture));
+Console.WriteLine("Dados Atualizados " + funcionario);
+
